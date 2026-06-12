e722a5cc301d0d97e8f4b9da0f182c9473c1e845
razbillions <razbillions@gmail.com>
2026-06-12 13:35:17 +0000

    Change supabase import to use dist/index.mjs ES module

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
-import { createClient } from '@supabase/supabase-js/dist/index.js'
+import { createClient } from '@supabase/supabase-js/dist/index.mjs'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
